5029769d6c2a915432c4bdc2f9d5317cfc8e87f5
Mrlunsuper <irisslee95@gmail.com>
2022-08-18 23:28:53 +0700

    Added dev tools

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6 */ 
+const gulp = require('gulp');
+const gSass = require('gulp-sass');
+const nSaas = require('node-sass');
+const sass = gSass(nSaas);
+
+gulp.task('sass', function() {
+    return gulp.src('./assets/scss/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('assets/css'));
+    }
+);
+
+gulp.task('watch', function() {
+    gulp.watch('./assets/scss/**/*.scss', gulp.series('sass'));
+});
